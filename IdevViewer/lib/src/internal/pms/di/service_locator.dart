0b144e84585ddebe77c6d936191035fc551cedb8
ggchae <ggchae@digicaps.com>
2025-10-26 13:12:15 +0900

    🔧 Register HomeRepo in initViewerServiceLocator
    - Add HomeRepo import
    - Register HomeRepo as LazySingleton in initViewerServiceLocator
    - This fixes GetIt error: HomeRepo is not registered
    - Allows viewer mode to access HomeRepo via Service Locator

---
+++ IdevViewer/lib/src/internal/pms/di/service_locator.dart
@@ -0,0 +1,30 @@
+import 'package:get_it/get_it.dart';
+import '../../core/api/api_client.dart';
+import '../../core/api/api_service.dart';
+import '../../repo/app_streams.dart';
+import '../../repo/home_repo.dart';
+
+final sl = GetIt.instance;
+
+void initServiceLocator() {
+  // AppStreams 등록 (LazySingleton으로 등록)
+  sl.registerLazySingleton<AppStreams>(() => AppStreams());
+
+  //ApiClient
+  sl.registerLazySingleton<ApiClient>(() => ApiClient());
+  sl.registerLazySingleton<ApiService>(
+      () => ApiService(apiClient: sl<ApiClient>()));
+}
+
+void initViewerServiceLocator() {
+  // 뷰어 모드에서는 AppStreams 등록하지 않음
+  // 최소한의 API 서비스만 등록
+
+  //ApiClient
+  sl.registerLazySingleton<ApiClient>(() => ApiClient());
+  sl.registerLazySingleton<ApiService>(
+      () => ApiService(apiClient: sl<ApiClient>()));
+  
+  // 뷰어 모드에서는 HomeRepo도 등록
+  sl.registerLazySingleton<HomeRepo>(() => HomeRepo());
+}
